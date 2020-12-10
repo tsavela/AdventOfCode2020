8021a40210b0b4e0623e9faec05a0a63f9cb10f7
Tommy Savela <tommy.savela@gmail.com>
2020-12-10 17:10:58 +0100

    Day 4

---
+++ Implementation/PassportProcessing.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Implementation
+{
+    public class PassportProcessing
+    {
+        private static readonly (string Key, Func<string, bool> Validator)[] RequiredFields = new (string Key, Func<string, bool> Validator)[]
+        {
+            ("byr", s => ValidateIntRange(s, 1920, 2002)),
+            ("iyr", s => ValidateIntRange(s, 2010, 2020)),
+            ("eyr", s => ValidateIntRange(s, 2020, 2030)),
+            ("hgt", s =>
+            {
+                if (s.Length < 4) return false;
+                var suffix = s.Substring(s.Length - 2, 2);
+                var value = s.Substring(0, s.Length - 2);
+                return suffix switch
+                {
+                    "cm" => ValidateIntRange(value, 150, 193),
+                    "in" => ValidateIntRange(value, 59, 76),
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }),
+            ("hcl", s =>
+            {
+                var regex = new Regex("^#[0-9a-f]{6}$");
+                return regex.IsMatch(s);
+            }),
+            ("ecl", s =>
+            {
+                return new[] {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}.Contains(s);
+            }),
+            ("pid", s =>
+            {
+                var regex = new Regex("^[0-9]{9}$");
+                return regex.IsMatch(s);
+            })
+        };
+
+        private static bool ValidateIntRange(string s, int min, int max)
+        {
+            if (!int.TryParse(s, out var value)) return false;
+            return value >= min && value <= max;
+        }
+
+        public bool ValidatePassport(PassportData[] passportData)
+        {
+            return RequiredFields.All(field => passportData.Any(pd => pd.Key == field.Key && field.Validator(pd.Value)));
+        }
+
+        public PassportData[][] ParseInputData(string input)
+        {
+            var blocks = input.Split("\n\n");
+            return blocks.Select(block =>
+            {
+                var keyAndValues = block.Split(' ', '\n');
+                return keyAndValues.Where(kv => kv != "").Select(kv =>
+                {
+                    var keyAndValue = kv.Split(':');
+                    return new PassportData(keyAndValue[0], keyAndValue[1]);
+                }).ToArray();
+            }).ToArray();
+        }
+    }
+
+    public record PassportData(string Key, string Value);
+}
---
+++ Tests/Day4Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Linq;
+using AutoFixture;
+using FluentAssertions;
+using Implementation;
+using Xunit;
+
+namespace Tests
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void ValidatePassport_ShouldReturnTrue_WhenGivenValidPassport()
+        {
+            var sut = new PassportProcessing();
+
+            var actual = sut.ValidatePassport(sut.ParseInputData("pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f")[0]);
+
+            actual.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValidatePassport_ShouldReturnFalse_WhenGivenPassportWithMissingRequiredData()
+        {
+            var sut = new PassportProcessing();
+
+            var actual = sut.ValidatePassport(sut.ParseInputData("pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980")[0]);
+
+            actual.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ParseInputData_ShouldReturnCorrectData_WhenGivenOnePassport()
+        {
+            var sut = new PassportProcessing();
+
+            var actual = sut.ParseInputData("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm");
+
+            actual.Length.Should().Be(1);
+            actual.First().Length.Should().Be(8);
+        }
+
+        [Fact]
+        public void ValidatePassport_ShouldReturnNumberOfValidPassports_WhenIteratedOverInputData()
+        {
+            var input = File.ReadAllText("Inputs\\day4.txt");
+            var sut = new PassportProcessing();
+            var passportsData = sut.ParseInputData(input);
+
+            var count = passportsData.Count(pd => sut.ValidatePassport(pd));
+
+            count.Should().Be(101);
+        }
+
+        [Theory]
+        [InlineData("eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926")]
+        [InlineData("iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946")]
+        [InlineData("hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277")]
+        [InlineData("hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007")]
+        public void ValidatePassport_ShouldReturnFalse_WhenGivenDataValuesOutOfRange(string input)
+        {
+            var sut = new PassportProcessing();
+            var passportData = sut.ParseInputData(input)[0];
+
+            var actual = sut.ValidatePassport(passportData);
+
+            actual.Should().BeFalse();
+        }
+
+        private PassportData[] GeneratePassportData(params string[] keys)
+        {
+            var fixture = new Fixture();
+            return keys.Select(s => new PassportData(s,fixture.Create<string>())).ToArray();
+        }
+    }
+}
