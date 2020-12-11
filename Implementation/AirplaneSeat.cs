4b7024c9ff61bc8595b6fe277d27464b5f3c0185
Tommy Savela <tommy.savela@gmail.com>
2020-12-11 16:06:18 +0100

    Day 5

---
+++ Implementation/AirplaneSeat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Implementation
+{
+    public class Seat
+    {
+        private static readonly Regex SeatDefinitionRegex = new Regex("^[FB]{7}[LR]{3}$");
+
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        public int Id { get; private set; }
+
+        public Seat(string seatDefinition)
+        {
+            if (!SeatDefinitionRegex.IsMatch(seatDefinition))
+                throw new ArgumentException("Invalid seat definition", nameof(seatDefinition));
+
+            Row = CalculateRow(seatDefinition.Substring(0, 7), Enumerable.Range(0, 128).ToArray());
+            Column = CalculateColumn(seatDefinition.Substring(7), Enumerable.Range(0, 8).ToArray());
+            Id = CalculateId(Row, Column);
+        }
+
+        private int CalculateId(in int row, in int column)
+        {
+            return row * 8 + column;
+        }
+
+        private int CalculateColumn(string columnDefinition, int[] availableColumns)
+        {
+            return CalculateInternal(columnDefinition, availableColumns, 'L');
+        }
+
+        private int CalculateRow(string rowDefinition, int[] availableRows)
+        {
+            return CalculateInternal(rowDefinition, availableRows, 'F');
+        }
+
+        private int CalculateInternal(string definition, int[] available, char firstPartSelector)
+        {
+            var partition = GetPartition(available, definition[0]);
+            if (definition.Length == 1) return partition.Single();
+            return CalculateInternal(definition.Substring(1), partition, firstPartSelector);
+
+            int[] GetPartition(int[] rows, char selector)
+            {
+                var startIndex = selector == firstPartSelector ? 0 : rows.Length / 2;
+                return rows.Skip(startIndex).Take(rows.Length / 2).ToArray();
+            }
+        }
+    }
+}
---
+++ Tests/Day5Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Implementation;
+using Xunit;
+
+namespace Tests
+{
+    public class Day5Tests
+    {
+        [Theory]
+        [InlineData("BFFFBBFRRR", 70, 7, 567)]
+        [InlineData("FFFBBBFRRR", 14, 7, 119)]
+        [InlineData("BBFFBBFRLL", 102, 4, 820)]
+        public void Properties_ShouldBeCalculatedCorrectly_GivenValidInput(string definition, int expectedRow, int expectedColumn, int expectedId)
+        {
+            var sut = new Seat(definition);
+
+            sut.Row.Should().Be(expectedRow);
+            sut.Column.Should().Be(expectedColumn);
+            sut.Id.Should().Be(expectedId);
+        }
+
+        [Fact]
+        public void CalculateMaxIdFromInput()
+        {
+            var input = File.ReadLines("Inputs\\day5.txt");
+
+            var max = input.Max(seatDefinition => new Seat(seatDefinition).Id);
+
+            max.Should().Be(976);
+        }
+
+        [Fact]
+        public void FindMySeat()
+        {
+            var input = File.ReadLines("Inputs\\day5.txt");
+            var occupiedSeats = input.Select(seatDefinition => new Seat(seatDefinition).Id).OrderBy(id => id).ToArray();
+            var possibleSeatIds = Enumerable.Range(0, 128 * 8);
+
+            var freeSeatId = possibleSeatIds.Single(seatId =>
+                !occupiedSeats.Contains(seatId) && occupiedSeats.Contains(seatId - 1) && occupiedSeats.Contains(seatId + 1));
+
+            freeSeatId.Should().Be(685);
+        }
+    }
+}
