42b7bd3da840a4210c4ae795821158de99574fd0
Tommy Savela <tommy.savela@gmail.com>
2020-12-14 22:35:04 +0100

    Day 14

---
+++ Implementation/DockingData.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Implementation
+{
+    public class DockingDataParser
+    {
+        public static (Memory.ValueMask Mask, ulong Address, ulong Value) ParseV1(string input)
+        {
+            var parts = input.Split(" = ");
+            if (parts[0] == "mask")
+            {
+                return (new Memory.ValueMask(parts[1]), default, default);
+            }
+
+            var val = parts[0][(parts[0].IndexOf('[')+1)..^1];
+            var address = ulong.Parse(val);
+            return (default, address, ulong.Parse(parts[1]));
+        }
+
+        public static (Memory.AddressMask Mask, ulong Address, ulong Value) ParseV2(string input)
+        {
+            var parts = input.Split(" = ");
+            if (parts[0] == "mask")
+            {
+                return (new Memory.AddressMask(parts[1]), default, default);
+            }
+
+            var val = parts[0][(parts[0].IndexOf('[')+1)..^1];
+            var address = ulong.Parse(val);
+            return (default, address, ulong.Parse(parts[1]));
+        }
+    }
+
+    public class Memory
+    {
+        public IDictionary<ulong, ulong> MemoryContents { get; } = new Dictionary<ulong, ulong>();
+
+        public ulong ApplyValueV1(ulong address, ValueMask valueMask, ulong memoryValue)
+        {
+            var value = valueMask.ApplyOn(memoryValue);
+            MemoryContents[address] = value;
+            return value;
+        }
+
+        public ulong[] ApplyValueV2(ulong address, AddressMask mask, ulong value)
+        {
+            var addressWildcard = mask.GetAddressWildcard(address);
+            var allAddresses = CalculateAllAddresses(addressWildcard);
+            foreach (var calculatedAddress in allAddresses)
+            {
+                MemoryContents[calculatedAddress] = value;
+            }
+
+            return allAddresses;
+        }
+
+        private ulong[] CalculateAllAddresses(string addressWildcard)
+        {
+            var indexOfFirstWildcard = addressWildcard.IndexOf('X');
+            if (indexOfFirstWildcard == -1) return new[] {Convert.ToUInt64(addressWildcard, 2)};
+
+            return new[] {'0', '1'}
+                .SelectMany(fix => CalculateAllAddresses(FixWildcard(addressWildcard, indexOfFirstWildcard, fix)))
+                .ToArray();
+            }
+
+        private string FixWildcard(string addressWildcard, in int indexOfFirstWildcard, in char fixChar)
+        {
+            return addressWildcard.Substring(0, indexOfFirstWildcard) + fixChar + addressWildcard.Substring(indexOfFirstWildcard + 1);
+        }
+
+        public class AddressMask
+        {
+            private readonly string _mask;
+
+            public AddressMask(string mask)
+            {
+                _mask = mask;
+            }
+
+            public string GetAddressWildcard(ulong address)
+            {
+                var sb = new StringBuilder();
+                var binaryAddress = Convert.ToString((long) address, 2).PadLeft(36, '0');
+                for (var i = 0; i < _mask.Length; i++)
+                {
+                    sb.Append(_mask[i] switch
+                    {
+                        '0' => binaryAddress[i],
+                        '1' => '1',
+                        'X' => 'X',
+                        _ => throw new ArgumentOutOfRangeException()
+                    });
+                }
+
+                return sb.ToString();
+            }
+
+            public static implicit operator AddressMask(string value) => new AddressMask(value);
+        }
+
+        public class ValueMask
+        {
+            private readonly ulong _orMask = 0;
+            private readonly ulong _andMask = 68719476735UL;
+
+            public string MaskString { get; }
+            public string OrString => Convert.ToString((long) _orMask, 2).PadLeft(36, '0');
+            public string AndString => Convert.ToString((long) _andMask, 2).PadLeft(36, '0');
+
+            public ValueMask(string value)
+            {
+                MaskString = value;
+                for (var bitPosition = value.Length - 1; bitPosition >= 0; bitPosition--)
+                {
+                    var bitChar = value[bitPosition];
+                    var power = 35 - bitPosition;
+                    var bitValue = (ulong) 1 << power;
+                    if (bitChar == '0') _andMask -= bitValue;
+                    if (bitChar == '1') _orMask += bitValue;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"And: {AndString} / Or: {OrString}";
+            }
+
+            public ulong ApplyOn(ulong value)
+            {
+                return value & _andMask | _orMask;
+            }
+
+            public static implicit operator ValueMask(string value) => new ValueMask(value);
+        }
+    }
+}
---
+++ Tests/Day14Tests.cs
@@ -0,0 +1,99 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Implementation;
+using Xunit;
+
+namespace Tests
+{
+    public class Day14Tests
+    {
+        [Theory]
+        [InlineData(11, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 73)]
+        [InlineData(101, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 101)]
+        [InlineData(0, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 64)]
+        [InlineData(0, "100XX1111X0011100X01000X010000000101", 36387750917)]
+        public void ApplyValueV1_ShouldGiveCorrectValue_GivenSpecificMask(ulong value, string mask, ulong expected)
+        {
+            var sut = new Memory();
+
+            var actual = sut.ApplyValueV1(1, mask, value);
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ParseData_ShouldReturnMask_WhenInputIsMask()
+        {
+            var actual = DockingDataParser.ParseV1("mask = 110100X1X01011X01X0X000111X00XX1010X");
+            actual.Mask.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ParseData_ShouldReturnAddressAndValue_WhenInputIsMemoryAssignment()
+        {
+            var actual = DockingDataParser.ParseV1("mem[29267] = 4155");
+            actual.Address.Should().Be(29267);
+            actual.Value.Should().Be(4155);
+        }
+
+        [Fact]
+        public void CalculateSumV1()
+        {
+            var input = File.ReadAllLines("Inputs\\day14.txt");
+            var memory = new Memory();
+            Memory.ValueMask valueMask = null;
+
+            foreach (var value in input.Where(i => !string.IsNullOrWhiteSpace(i)))
+            {
+                var parsed = DockingDataParser.ParseV1(value);
+                if (parsed.Mask != null) valueMask = parsed.Mask;
+                else memory.ApplyValueV1(parsed.Address, valueMask, parsed.Value);
+            }
+
+            ulong result = 0;
+            foreach (var value in memory.MemoryContents.Values)
+            {
+                result += value;
+            }
+            result.Should().Be(17934269678453);
+        }
+
+        [Theory]
+        [InlineData(42, "000000000000000000000000000000X1001X", 4)]
+        [InlineData(26, "00000000000000000000000000000000X0XX", 8)]
+        [InlineData(26, "000000000000000000000000000000000000", 1)]
+        public void ApplyValueV2_ShouldReturnAppliedAddresses_WhenGivenInput(ulong address, string mask, int expectedAddressCount)
+        {
+            var sut = new Memory();
+
+            var actual = sut.ApplyValueV2(address, mask, 0);
+
+            actual.Length.Should().Be(expectedAddressCount);
+            sut.MemoryContents.Count.Should().Be(expectedAddressCount);
+        }
+
+        [Fact]
+        public void CalculateSumV2()
+        {
+            var input = File.ReadAllLines("Inputs\\day14.txt");
+            var memory = new Memory();
+            Memory.AddressMask valueMask = null;
+
+            foreach (var value in input.Where(i => !string.IsNullOrWhiteSpace(i)))
+            {
+                var parsed = DockingDataParser.ParseV2(value);
+                if (parsed.Mask != null) valueMask = parsed.Mask;
+                else memory.ApplyValueV2(parsed.Address, valueMask, parsed.Value);
+            }
+
+            ulong result = 0;
+            foreach (var value in memory.MemoryContents.Values)
+            {
+                result += value;
+            }
+            result.Should().Be(3440662844064);
+        }
+
+    }
+}
