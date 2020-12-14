df7d1ce666e5d955aee28b630c991e68dbb0fb1a
Tommy Savela <tommy.savela@gmail.com>
2020-12-14 15:30:57 +0100

    Day 6

---
+++ Implementation/CustomsFormCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Implementation
+{
+    public class CustomsFormCalculator
+    {
+        public string CalculateAnswersSomeoneAnsweredYesOn(string[] input)
+        {
+            return string.Concat(input.SelectMany(i => i.ToArray()).Distinct().OrderBy(i => i));
+        }
+
+        public string CalculateAnswersEveryoneAnsweredYesOn(string[] input)
+        {
+            var possibleAnswers = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            return string.Concat(possibleAnswers.Where(answer => input.Where(input => input.Length > 0).All(input => input.ToLower().Contains(answer))));
+        }
+    }
+}
---
+++ Tests/Day6Tests.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using FluentAssertions;
+using Implementation;
+using Xunit;
+
+namespace Tests
+{
+    public class Day6Tests
+    {
+        [Fact]
+        public void CalculateAnswersSomeoneAnsweredYesOn_ShouldGiveCorrectValue_GivenSpecificValidInput()
+        {
+            var input = new[] {"abcx", "abcy", "abcz"};
+            var sut = new CustomsFormCalculator();
+
+            var actual = sut.CalculateAnswersSomeoneAnsweredYesOn(input);
+
+            actual.Should().Be("abcxyz");
+        }
+
+        [Fact]
+        public void SumOfCalculateAnswersSomeoneAnsweredYesOn()
+        {
+            var input = File.ReadAllText("Inputs\\day6.txt");
+            var inputGroups = input.Split("\n\n");
+            var sut = new CustomsFormCalculator();
+
+            var sum = inputGroups.Sum(inputGroup => sut.CalculateAnswersSomeoneAnsweredYesOn(inputGroup.Split('\n')).Length);
+
+            sum.Should().Be(6504);
+        }
+
+        [Fact]
+        public void CalculateAnswersEveryoneAnsweredYesOn_ShouldGiveCorrectValue_GivenSpecificValidInput()
+        {
+            var input = new[] {"ab", "ac"};
+            var sut = new CustomsFormCalculator();
+
+            var actual = sut.CalculateAnswersEveryoneAnsweredYesOn(input);
+
+            actual.Should().Be("a");
+        }
+
+        [Fact]
+        public void SumOfCalculateAnswersEveryoneAnsweredYesOn()
+        {
+            var input = File.ReadAllText("Inputs\\day6.txt");
+            var inputGroups = input.Split("\n\n");
+            var sut = new CustomsFormCalculator();
+
+            var sum = inputGroups.Sum(inputGroup => sut.CalculateAnswersEveryoneAnsweredYesOn(inputGroup.Split('\n')).Length);
+
+            sum.Should().Be(3351);
+        }
+    }
+}
